31e4d3135f4ab881dd01bfc67e12b107365551f0
Yulia-Kot <yuhalaniuk@ukr.net>
2023-08-25 12:06:40 +0300

    upd forms

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const menuEl = document.querySelector("div[js-mobile-menu]");
 
 openBtnRef.addEventListener("click", (e) => {
   menuEl.classList.remove("is-hidden");
-  //   document.body.style.overflow = "hidden";
+  document.body.style.overflow = "hidden";
 });
 
 closeBtnRef.addEventListener("click", (e) => {
   menuEl.classList.add("is-hidden");
-  //   document.body.style.overflow = "auto";
+  document.body.style.overflow = "auto";
 });
 
 //  -----------------------------------------
@@ -29,11 +29,12 @@ const modalEl = document.querySelector(".modal");
 openModalnRef.forEach((btn) =>
   btn.addEventListener("click", (e) => {
     modalEl.classList.remove("is-hidden");
-    //   document.body.style.overflow = "hidden";
+    document.body.style.overflow = "hidden";
 
     overlayEl.addEventListener("click", (e) => {
       if (e.target === e.currentTarget) {
         modalEl.classList.add("is-hidden");
+        document.body.style.overflow = "auto";
       }
     });
   })
@@ -41,16 +42,18 @@ openModalnRef.forEach((btn) =>
 
 // --------------------------------------------
 
-const formRef = document.querySelector(".modal-form");
+const modalFormRef = document.querySelector(".modal-form");
 
-formRef.addEventListener("submit", (e) => {
+modalFormRef.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const { name, number, mySelect } = e.currentTarget.elements;
+  const { name, number, mySelectBarber, mySelectService } =
+    e.currentTarget.elements;
   const userData = {
     name: name.value,
     number: number.value,
-    barber: mySelect.value,
+    barber: mySelectBarber.value,
+    service: mySelectService.value,
   };
 
   console.log(userData);
@@ -58,6 +61,7 @@ formRef.addEventListener("submit", (e) => {
   e.currentTarget.reset();
 
   modalEl.classList.add("is-hidden");
+  thanksModal.classList.remove("is-hidden");
 });
 
 //  -----------------------------------------
@@ -66,9 +70,15 @@ const backBtnRef = document.querySelector("[js-hero-btn-back]");
 const forwardBtnRef = document.querySelector("[js-hero-btn-forward]");
 const heroWrapEl = document.querySelector(".hero-wrap-background");
 
+const lineOne = document.querySelector(".hero-back-one");
+const lineTwo = document.querySelector(".hero-back-two");
+const lineThree = document.querySelector(".hero-back-three");
+
 const navBtnClassToggle = (activeBtn, otherBtn) => {
+  console.log(heroWrapEl.classList.contains("is-back"));
   if (heroWrapEl.classList.contains(otherBtn)) {
     heroWrapEl.classList.remove(otherBtn);
+
     return;
   }
 
@@ -81,10 +91,60 @@ const navBtnClassToggle = (activeBtn, otherBtn) => {
   heroWrapEl.classList.add(activeBtn);
 };
 
-backBtnRef.addEventListener("click", (e) =>
-  navBtnClassToggle("is-back", "is-forw")
-);
+const navLinesActiveClassToggle = () => {
+  lineOne.classList.remove("is-active");
+  lineTwo.classList.remove("is-active");
+  lineThree.classList.remove("is-active");
+  if (heroWrapEl.classList.contains("is-back")) {
+    lineOne.classList.add("is-active");
+  } else if (heroWrapEl.classList.contains("is-forw")) {
+    lineThree.classList.add("is-active");
+  } else {
+    lineTwo.classList.add("is-active");
+  }
+};
 
-forwardBtnRef.addEventListener("click", (e) =>
-  navBtnClassToggle("is-forw", "is-back")
-);
+backBtnRef.addEventListener("click", (e) => {
+  navBtnClassToggle("is-back", "is-forw");
+  navLinesActiveClassToggle();
+});
+
+forwardBtnRef.addEventListener("click", (e) => {
+  navBtnClassToggle("is-forw", "is-back");
+  navLinesActiveClassToggle();
+});
+
+// -----------------------------------------
+
+const contactFormRef = document.querySelector(".contact-form");
+
+contactFormRef.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const { userName, phone, comment } = e.currentTarget.elements;
+
+  const userData = {
+    name: userName.value,
+    number: phone.value,
+    comment: comment.value,
+  };
+
+  console.log(userData);
+
+  e.currentTarget.reset();
+  thanksModal.classList.remove("is-hidden");
+});
+
+// -----------------------------------------
+
+const thanksModal = document.querySelector(".modal-gratitude");
+
+const overlayThanksEl = document.querySelector("[js-modal-gratitude-backdrop]");
+
+overlayThanksEl.addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target === e.currentTarget) {
+    thanksModal.classList.add("is-hidden");
+    document.body.style.overflow = "auto";
+  }
+});
